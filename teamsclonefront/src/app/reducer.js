00ab03cb8d9a451a4fd531c244d00476af26a61f
shobit <shobitnair10@gmail.com>
2021-06-26 03:48:40 +0530

    front end setup

---
+++ teamsclonefront/src/app/reducer.js
@@ -0,0 +1,17 @@
+//REDUx State Management.
+
+
+
+const initState = {
+    identity:"",
+    isHost:"",
+}
+
+const reducer = (state = initState , action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
+
+export default reducer
