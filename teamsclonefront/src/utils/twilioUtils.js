44da8218e034c0d920b1396272eeff601e3e962f
shobit <shobitnair10@gmail.com>
2021-06-27 15:29:06 +0530

    RoomPage and UI fixes

--- teamsclonefront/src/utils/twilioUtils.js
+++ teamsclonefront/src/utils/twilioUtils.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const checkIfRoomExists = async (roomId) => {
   const response = await axios.get(
-    `https://zoom-clone-service-3493-dev.twil.io/room-exists?roomId=${roomId}`
+    `https://teamscloneservice-7248-dev.twil.io/room-exists?roomId=${roomId}`
   );
 
   return response.data.roomExists;
