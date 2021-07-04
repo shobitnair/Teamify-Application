d2f003b0b0f56dde4e5c6bb3ba9b57ea846acd26
shobit <shobitnair10@gmail.com>
2021-07-04 16:48:13 +0530

    mobile responsiveness improved

--- teamsclonefront/src/RoomPage/RoomPageMobile.js
+++ teamsclonefront/src/RoomPage/RoomPageMobile.js
@@ -2,35 +2,48 @@ import React, { useState } from "react";
 import ParticipantsSection from "./ParticipantsSection/ParticipantsSection";
 import VideoSection from "./VideoSection/VideoSection";
 import ChatSection from "./ChatSection/ChatSection";
-import { Grid , Button } from "@material-ui/core";
-import roomIcon from '@material-ui/icons/SettingsInputSvideo';
+import { Grid, Button } from "@material-ui/core";
+
 const RoomPageMobile = () => {
+  //Page Display state
   const [page, setPage] = useState("video");
 
   return (
-      <>
-    <Grid container direction="row" id="page" style={{ height: "90vh" }}>
+    <>
+      <Grid container direction="row" id="page" style={{ height: "90vh" }}>
+        <Grid item xs={12} hidden={page !== "participant"}>
+          <div id="participant">
+            <ParticipantsSection />
+          </div>
+        </Grid>
 
-      <Grid item xs={12} hidden={page !== "participant"}>
-        <div id="participant">
-          <ParticipantsSection />
-        </div>
+        <Grid item xs={12} hidden={page !== "video"}>
+          <div id="room">
+            <VideoSection />
+          </div>
+        </Grid>
+
+        <Grid item xs={12} hidden={page !== "message"}>
+          <div id="chat">
+            <ChatSection />
+          </div>
+        </Grid>
       </Grid>
 
-      <Grid item xs={12} hidden={page !== "video"}>
-        <div id="room">
-          <VideoSection />
-        </div>
-      </Grid>
-
-      <Grid item xs={12} hidden={page !== "message"}>
-        <div id="chat">
-          <ChatSection />
-        </div>
-      </Grid>
-    </Grid>
-    
-    
+      <div id="navbar">
+        <Button id="nav_bt" onClick={() => setPage("participant")}>
+          <i class="fas fa-id-card-alt"></i>
+        </Button>
+        <Button id="nav_bt" onClick={() => setPage("video")}>
+          <i class="fab fa-joomla"></i>
+        </Button>
+        <Button id="nav_bt" onClick={() => setPage("message")}>
+          <i class="fas fa-comment-alt"></i>
+        </Button>
+        <Button id="nav_bt">
+          <i class="fas fa-bars"></i>
+        </Button>
+      </div>
     </>
   );
 };
